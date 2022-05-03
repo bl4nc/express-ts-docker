2e3f23680c433af4d96816f211422aba3768f325
Rafael Araújo Rocha <40602200+bl4nc@users.noreply.github.com>
2022-05-03 19:51:06 -0300

    Delete build directory

--- build/router.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.router = void 0;
-const express_1 = require("express");
-const HelloWorld_1 = require("./Controllers/HelloWorld");
-const router = (0, express_1.Router)();
-exports.router = router;
-const helloWorld = new HelloWorld_1.HelloWorld();
-router.get("/", helloWorld.Hello);
